4b551352bbbc5085d8d987a57b1cc737e7027021
Daniel Kneale <82185529+Crimson-Beast@users.noreply.github.com>
2022-03-24 18:42:20 -0700

    set up exotics table migration and seed, implimented scripts

--- data/migrations/20220325010729_first_migration.js
+++ data/migrations/20220325010729_first_migration.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex) {
-  
+  return knex.scheme.createTable("exotics", tbl =>{
+      tbl.increments();
+      tbl.string("Name", 25).unique().notNullable()
+      tbl.string("Weapontype", 20).notNullable()
+      tbl.string("AmmoType", 25).notNullable()
+      tbl.string("ExoticPerk").notNullable()
+  })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTableIfExists("exotics");
 };
